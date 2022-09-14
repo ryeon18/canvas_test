106f84d9e2aab0295b542bf8a6fa6d54c1a6b3e7
sy <sy.park@weballin.com>
2022-09-14 17:39:54 +0900

    Add: brush canvas border

--- canvas/src/pages/index.js
+++ canvas/src/pages/index.js
@@ -75,7 +75,6 @@ const Home = () => {
     }
 
     function setMouseWheel(e) {
-        e.preventDefault();
         console.log('setMouseWheel');
         var xs = (e.clientX - pointX) / scale,
             ys = (e.clientY - pointY) / scale,
@@ -85,6 +84,76 @@ const Home = () => {
         pointY = e.clientY - ys * scale;
 
         setTransform();
+    }
+
+    const imgID = document.getElementById('imgID');
+    const resultID = document.getElementById('resultID');
+
+    function imageZoom(imgID, resultID) {
+        var img, lens, result, cx, cy;
+        img = document.getElementById(imgID);
+        result = document.getElementById(resultID);
+        /* Create lens: */
+        lens = document.createElement('DIV');
+        lens.setAttribute('class', 'img-zoom-lens');
+        /* Insert lens: */
+        img.parentElement.insertBefore(lens, img);
+        /* Calculate the ratio between result DIV and lens: */
+        cx = result.offsetWidth / lens.offsetWidth;
+        cy = result.offsetHeight / lens.offsetHeight;
+        /* Set background properties for the result DIV */
+        result.style.backgroundImage = "url('" + img.src + "')";
+        result.style.backgroundSize = img.width * cx + 'px ' + img.height * cy + 'px';
+        /* Execute a function when someone moves the cursor over the image, or the lens: */
+        lens.addEventListener('mousemove', moveLens);
+        img.addEventListener('mousemove', moveLens);
+        /* And also for touch screens: */
+        lens.addEventListener('touchmove', moveLens);
+        img.addEventListener('touchmove', moveLens);
+        function moveLens(e) {
+            var pos, x, y;
+            /* Prevent any other actions that may occur when moving over the image */
+            e.preventDefault();
+            /* Get the cursor's x and y positions: */
+            pos = getCursorPos(e);
+            /* Calculate the position of the lens: */
+            x = pos.x - lens.offsetWidth / 2;
+            y = pos.y - lens.offsetHeight / 2;
+            /* Prevent the lens from being positioned outside the image: */
+            if (x > img.width - lens.offsetWidth) {
+                x = img.width - lens.offsetWidth;
+            }
+            if (x < 0) {
+                x = 0;
+            }
+            if (y > img.height - lens.offsetHeight) {
+                y = img.height - lens.offsetHeight;
+            }
+            if (y < 0) {
+                y = 0;
+            }
+            /* Set the position of the lens: */
+            lens.style.left = x + 'px';
+            lens.style.top = y + 'px';
+            /* Display what the lens "sees": */
+            result.style.backgroundPosition = '-' + x * cx + 'px -' + y * cy + 'px';
+        }
+
+        function getCursorPos(e) {
+            var a,
+                x = 0,
+                y = 0;
+            e = e || window.event;
+            /* Get the x and y positions of the image: */
+            a = img.getBoundingClientRect();
+            /* Calculate the cursor's x and y coordinates, relative to the image: */
+            x = e.pageX - a.left;
+            y = e.pageY - a.top;
+            /* Consider any page scrolling: */
+            x = x - window.pageXOffset;
+            y = y - window.pageYOffset;
+            return {x: x, y: y};
+        }
     }
 
     return (
@@ -115,6 +184,17 @@ const Home = () => {
                 </div>
             </div>
             <h2> 이미지 프리뷰 </h2>
+            <div className="img-zoom-container">
+                <img
+                    id="myimage"
+                    src={disney1}
+                    width="300"
+                    height="240"
+                    alt="Girl"
+                    onMouseOver={() => imageZoom(imgID, resultID)}
+                />
+                <div id="myresult" className="img-zoom-result"></div>
+            </div>
         </div>
     );
 };
